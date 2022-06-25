74b7ac53a1a282915814503fcc0e5d344cad2da9
Neiva-Sartori-Representacoes <neiva26.sartori@outlook.com>
2022-06-25 01:13:20 -0300

    add produto

---
+++ js/data.js
@@ -0,0 +1,37 @@
+export const lista = [
+    {   descricao: "Roupa infantil feminina",
+        tamanho: "Tamanho: M | P",
+        imagem: "./img/01.png",
+        preco: "50,00",
+        ref: "0dsf4sd1v0255",
+        cores: ["red", "blue"]  
+    },
+    {   descricao: "Roupa infantil feminina",
+        tamanho: "Tamanho: M | P",
+        imagem: "./img/02.png",
+        preco: "50,00",
+        ref: "0dsf4sd1v0255",
+        cores: ["red", "blue"]  
+    },
+    {   descricao: "Roupa infantil feminina",
+        tamanho: "Tamanho: M | P",       
+        imagem: "./img/03.png",
+        preco: "50,00",
+        ref: "0dsf4sd1v0255",
+        cores: ["red", "blue"]  
+    },
+    {   descricao: "Roupa infantil feminina",
+        tamanho: "Tamanho: M | P",
+        imagem: "./img/04.png",
+        preco: "50,00",
+        ref: "0dsf4sd1v0255",
+        cores: ["red", "blue"]  
+    },
+    {   descricao: "Roupa infantil feminina22222",
+        tamanho: "Tamanho: M | P",
+        imagem: "./img/05.png",
+        preco: "50,00",
+        ref: "0dsf4sd1v0255",
+        cores: ["white", "blue"]  
+    }
+  ]
---
+++ js/index.js
@@ -0,0 +1,42 @@
+import { lista } from "./data.js";
+
+export function cards(produtos) {
+  const main = document.getElementById('main')
+  let cards = ''
+  let coresP = ''
+  produtos.map(produto => {
+    cards += `
+    <div class="card">
+      <img src="${produto.imagem}" alt="imagem de uma roupa"/>
+      <div class="card__infos">
+        <p class="card__text">${produto.descricao}</p>
+        <p class="card__text">${produto.tamanho}</p>
+        <div class="cores">
+          <span>Cores: </span>`
+    produto.cores.map(cor => {
+      cards += `<div class="bullet" style="background-color:${cor}"></div>`
+    })
+    cards +=`
+    </div>
+      <p class="ref">Ref: <span>${produto.ref}</span></p>
+      <p class="preco">Preço: <span>R$ ${produto.preco}</span></p>
+      </div>
+      </img>
+    </div>`
+    console.log(cards);
+  })
+  main.innerHTML = cards;
+}
+
+
+export function nav(paths) {
+
+
+}
+
+window.onload = async function () {
+
+  await cards(lista)
+}
+
+
